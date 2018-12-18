8551d8fa8f64e19520e4923ef01e74438274add4
Isa Kalinsaz <isa.kalinsaz@kuehne-nagel.com>
2018-12-18 20:12:03 +0200

    basic structure created

---
+++ src/main/java/com/sda/project/bookinglist/BookinglistApplication.java
@@ -0,0 +1,14 @@
+package com.sda.project.bookinglist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookinglistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookinglistApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/sda/project/bookinglist/BookinglistApplicationTests.java
@@ -0,0 +1,17 @@
+package com.sda.project.bookinglist;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BookinglistApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
